65374b7ff760fe0419127d1771ac07c78fa96250
Winx-Bloom <blessingoluwaseun2001@gmail.com>
2023-04-03 13:00:02 +0100

    first commit

---
+++ .vscode/csc.py
@@ -0,0 +1,13 @@
+# DATA types include Strings, Booleans and Integar
+#Strings undergoes concantination- i.e enclosed in quotation
+#Boolean are true and false statements
+# (single =) is to denoted an assignment 
+# double = fr validity
+# for example name = dolly
+# name is an identifier and the dolly must be enclosed in a string so that it can run
+#LISTS ARE enclosed in[]
+#Tuple are enclosed in()
+# Dictionary are enclosed in{}
+Dictionary has two things, key and value
+a = {"name" ;"dolapo", "age" ; 23 , "serious" ; false } 
+print(a.get(name))
